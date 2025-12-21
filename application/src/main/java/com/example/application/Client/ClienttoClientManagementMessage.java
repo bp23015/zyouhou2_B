027b72db8412cc653640ab15845fa38f0532eda1
Takamurakeisuke <bp23114@shibaura-it.ac.jp>
2025-12-22 00:18:00 +0900

    クライアントサーバの書き足し

--- application/src/main/java/com/example/application/Client/ClienttoClientManagementMessage.java
+++ application/src/main/java/com/example/application/Client/ClienttoClientManagementMessage.java
@@ -1,13 +1,13 @@
 package com.example.application.Client;
 
-public class ClienttoClientManagementMessage {
+public class ClientToClientManagementMessage {
     String userName;
     int passWord;
     String taskName;
     boolean authenticationFlag;
     int gameRecord;
 
-    public ClienttoClientManagementMessage(String userName,int passWord,String taskName,boolean authenticationFlag,int gameRecord){
+    public ClientToClientManagementMessage(String userName,int passWord,String taskName,boolean authenticationFlag,int gameRecord){
 this.userName=userName;
 this.passWord=passWord;
 this.taskName=taskName;
